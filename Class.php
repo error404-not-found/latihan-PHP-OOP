b60413e1dd41b50e0296265b70f1f48fde71a26d
error404-not-found <iphul.sl@gmail.com>
2022-02-03 20:44:01 +0900

    latihan materi abstrac

--- Class.php
+++ Class.php
@@ -1,31 +1,34 @@
 <?php
 
-class book{
+class book
+{
 
     // Attribut
     public  $name,
-            $publisher,
-            $price,
-            $stock;
+        $publisher,
+        $price,
+        $stock;
 
 
-    public function __construct(string $name,string $publisher,int $price,int $stock){
-        $this->name     =$name;
-        $this->publisher=$publisher;
-        $this->price    =$price;
-        $this->stock    =$stock;
+    public function __construct(string $name, string $publisher, int $price, int $stock)
+    {
+        $this->name     = $name;
+        $this->publisher = $publisher;
+        $this->price    = $price;
+        $this->stock    = $stock;
     }
-
 }
 
-class idSiswa{
+class idSiswa
+{
 
     // Attribut
     public  $name,
-            $nis,
-            $class;
+        $nis,
+        $class;
 
-    public function __construct(string $name,int $nis,string $class){
+    public function __construct(string $name, int $nis, string $class)
+    {
         $this->name     = $name;
         $this->nis      = $nis;
         $this->class    = $class;
@@ -34,23 +37,73 @@ class idSiswa{
 
 // namespace solving same class
 namespace idSiswa\one {
-    class idSiswa{
-        public function tes(){
+    class idSiswa
+    {
+        public function tes()
+        {
             echo "tes";
         }
     }
 
-    class idGuru{
-        public function no(){
+    class idGuru
+    {
+        public function no()
+        {
             echo "no";
         }
     }
 }
 
 namespace idSiswa\two {
-    class idSiswa{
-        public function yes(){
+    class idSiswa
+    {
+        public function yes()
+        {
             echo "yes";
+        }
+    }
+}
+
+
+namespace abstrac {
+
+    // class Biodatasiswa 
+    // {
+    //     public string   $nama,
+    //         $jenisKelamin,
+    //         $kelas,
+    //         $alamat;
+    // }
+
+
+    abstract class siswa
+    {
+
+        abstract public function namaLengkap(string $nama);
+        abstract public function jenisKelamin(string $jenisKelamin);
+        abstract public function kelas(string $kelas);
+        abstract public function alamat(string $alamat);
+    }
+
+
+    class sd extends siswa
+    {
+
+        public function namaLengkap($nama = "")
+        {
+            echo "nama lengkap: $nama" . PHP_EOL;
+        }
+        public function jenisKelamin($jenisKelamin = "")
+        {
+            echo "jenis kelamin: $jenisKelamin" . PHP_EOL;
+        }
+        public function kelas($kelas = "")
+        {
+            echo "kelas: $kelas" . PHP_EOL;
+        }
+        public function alamat($alamat = "")
+        {
+            echo "Alamt: $alamat" . PHP_EOL;
         }
     }
 }
--- Object.php
+++ Object.php
@@ -1,25 +1,34 @@
 <?php
 require_once "Class.php";
 
-$book = new book("","",0,0);
+$book = new book("", "", 0, 0);
 var_dump($book);
 
-$siswa = new idSiswa("",0,"");
+$siswa = new idSiswa("", 0, "");
 var_dump($siswa);
 
 //call namespace 
 $tes = new idSiswa\one\idSiswa();
-echo $tes->tes().PHP_EOL;
+echo $tes->tes() . PHP_EOL;
 
 $yes = new idSiswa\two\idSiswa();
-echo $yes->yes().PHP_EOL;
+echo $yes->yes() . PHP_EOL;
 
 // import namespace
-use idSiswa\one\ {idsiswa as siswa,idGuru as guru};
-$retes = new siswa();
-echo $retes->tes().PHP_EOL;
+use idSiswa\one\{idsiswa as murid, idGuru as guru}; //use alias (as)
+
+$retes = new murid();
+echo $retes->tes() . PHP_EOL;
 
 $retes2 = new guru();
 echo $retes2->no();
 
+use abstrac\{siswa, sd, smp};
 
+$biodataSiswaSd = new sd();
+
+
+$biodataSiswaSd->namaLengkap("wakando");
+$biodataSiswaSd->jenisKelamin("Laki");
+$biodataSiswaSd->kelas("enam");
+$biodataSiswaSd->alamat("Rusia");
